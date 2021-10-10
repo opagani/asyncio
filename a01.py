9f7635ea0065c74995ca469d98d586df774d1608
Reuven Lerner <reuven@lerner.co.il>
2021-10-10 18:46:28 +0300

    gitautosync update 36

--- a01.py
+++ a01.py
@@ -14,4 +14,6 @@ async def main():
 # how do we run it? We put it on the event loop
 # I don't put main, the function, on the event loop
 # rather, I put main(), the result of calling main, on the loop
-asyncio.run(main)
+print('Before')
+asyncio.run(main())
+print('After')
