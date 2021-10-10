1ab79573c09a7c3c4b9dd1bc6fa2653143bad370
Reuven Lerner <reuven@lerner.co.il>
2021-10-10 21:25:11 +0300

    gitautosync update 280

--- a13.py
+++ a13.py
@@ -19,16 +19,11 @@ async def vowel_counts(one_url):
 async def main:
     start_time = time.perf_counter()
 
-    tasks = []
     for one_url in ['https://lerner.co.il',
                     'https://python.org/',
                     'https://nytimes.com',
                     'https://postgresql.org']:
-        t = asyncio.create_task(get_one_site(one_url))
-        tasks.append(t)
 
-    # wait for all of the tasks in the "tasks" list to finish
-    await asyncio.gather(*tasks)
-    end_time = time.perf_counter()
+        await get_one_site(one_url)
 
     print(f'Total time = {end_time - start_time}')
