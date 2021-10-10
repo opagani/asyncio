bbefe7680b1d87a5e037c599781078ccf5326e9e
Reuven Lerner <reuven@lerner.co.il>
2021-10-10 20:40:00 +0300

    gitautosync update 188

---
+++ a09.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import asyncio
+
+
+async def hello():
+    print('Hello from before sleeping!')
+    asyncio.sleep(1)
+    print('Hello from after sleeping!')
+
+
+async def main():
+    await hello()
--- flycheck_a09.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import asyncio
-
-async def hello():
-    print('Hello from before !')
-
-async def main():
-    await hello()
