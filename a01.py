493c1cb060511f0858471ff762838eee4cf411f0
Reuven Lerner <reuven@lerner.co.il>
2021-10-10 18:41:12 +0300

    gitautosync update 20

---
+++ a01.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+# simplest possible asyncio program
+
+async def main():
+    print('Hello, world!')
---
+++ flycheck_a01.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+# simplest possible asyncio program
+
+async def main():
+    print('Hello, world!')
+
+    
