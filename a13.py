bd834a624b1b2f42d13dc1e6437a704a5b0a36cb
Reuven Lerner <reuven@lerner.co.il>
2021-10-10 21:24:22 +0300

    gitautosync update 276

--- a13.py
+++ a13.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python3
 
 import asyncio
+import aiohttp
+from collections import defaultdict
+
+counts = defaultdict(int)
 
 
 async def vowel_counts(one_url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(one_url) as response:
+            content = await response.text()
+            for one_character in content:
+                if one_character.lower() in 'aeiou':
+                    counts[one_url] += 1
