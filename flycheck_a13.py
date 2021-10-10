9004f2f6c2c8a19780d793e8294c0167b31c6ada
Reuven Lerner <reuven@lerner.co.il>
2021-10-10 21:23:45 +0300

    gitautosync update 273

---
+++ flycheck_a13.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import asyncio
+import aiohttp
+
+counts = {}
+
+
+async def vowel_counts(one_url):
+    async with aiohttp.ClientSession() as session:
+        async with session.get(one_url) as response:
+            content = await response.text()
+            total = 0
+            for one_character in content:
+                if one_character.lower() in 'aeiou':
+                    counts[one_url] +
