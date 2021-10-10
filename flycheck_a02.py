f5e7894634f6d7d134d012782d190acfdab5edf6
Reuven Lerner <reuven@lerner.co.il>
2021-10-10 18:48:33 +0300

    gitautosync update 38

---
+++ flycheck_a02.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+import asyncio
+
+# simplest possible asyncio program
+
+# this is a coroutine *function*
+# calling it returns a coroutine *object*
+
+
+
+async def main():
+    print('Hello, world!')
+
+# how do we run it? We put it on the event loop
+# I don't put main, the function, on the event loop
+# rather, I put main(), the result of calling main, on the loop
+print('Before')
+asyncio.run(main())
+print('After')
