427c79fb0265ee4642da92803dd0318b118a164b
Reuven Lerner <reuven@lerner.co.il>
2021-10-10 20:48:06 +0300

    gitautosync update 211

---
+++ flycheck_a10.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+import asyncio
+
+async def main():
+    for one_url in ['https://lerner.co.il',
+                    'https://python.org/',
+                    'https://nytimes.com',
+                    'https://washingtonpost.com'
